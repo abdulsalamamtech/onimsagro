5bb10e4b2829c0e9a7e2f95c30a1e07318fcfc78
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-04-17 15:05:55 +0100

    Modified routes

--- routes/apis/quest.php
+++ routes/apis/quest.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\AssetController;
 use App\Http\Controllers\ConsultationController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PaymentAccountController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 
 
@@ -22,6 +24,9 @@ Route::apiResource('payment-accounts', PaymentAccountController::class)
 // Consultation Routes
 Route::apiResource('consultations', ConsultationController::class)
 ->only(['store']);
+
+// Product Routes
+Route::get('products', ProductController::class, 'getProducts');
 
 // Order Routes
 Route::apiResource('orders', OrderController::class)
