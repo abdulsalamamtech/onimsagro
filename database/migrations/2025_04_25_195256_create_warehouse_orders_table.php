40fc4f53695f6d91fabe5d35f5505356e1b9cc68
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-04-25 21:22:04 +0100

    Add database migration

---
+++ database/migrations/2025_04_25_195256_create_warehouse_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('warehouse_orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('warehouse_id')->constrained('warehouses')->nullOnDelete();
+            $table->string('full_name');
+            $table->string('phone_number');
+            $table->string('email')->nullable();
+            $table->string('address')->nullable();
+            $table->decimal('total_price', 10, 2)->default(0);
+            $table->enum('status', ['pending', 'confirmed', 'processing', 'shipped', 'delivered', 'cancelled'])->default('pending');
+            $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnDelete();
+            $table->foreignId('updated_by')->nullable()->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('warehouse_orders');
+    }
+};
