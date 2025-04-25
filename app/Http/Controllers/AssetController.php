bd2943cfbcfc43c4518094a4d57444d6fce1a13f
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-04-25 21:43:24 +0100

    Modified file

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponse;
+use App\Http\Resources\AssetResource;
 use App\Models\Asset;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,14 @@ class AssetController extends Controller
         // Fetch all assets from your database
         $assets = Asset::latest()->paginate();
 
-        return ApiResponse::success($assets);
-
+        // return ApiResponse::success($assets);
+        // check if data is empty
+        if ($assets->isEmpty()) {
+            return ApiResponse::error([], "No assets found", 404);
+        }
+        // transform data
+        $response = AssetResource::collection($assets);
+        // return response
+        return ApiResponse::success($response, 'successful', 200); 
     }
 }
