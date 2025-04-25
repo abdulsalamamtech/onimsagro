1b2bc19c8cdcb4121d47afd6b4b4dd5139477271
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-04-25 21:22:26 +0100

    Add model

---
+++ app/Models/WarehouseOrder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class WarehouseOrder extends Model
+{
+    use HasFactory, SoftDeletes;
+    
+    protected $fillable = [
+        'warehouse_id',
+        'user_id',
+        'updated_by',
+        'full_name',
+        'email',
+        'phone_number',
+        'address',
+        'total_price',
+        'status',
+    ];
+    /**
+     * Get the user that owns the order.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    } 
+    
+    /**
+     * Get the warehouse for the order.
+     */
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    } 
+}
