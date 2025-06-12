3bcfa8e87581107102f9a87d72e05720816adbb9
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-12 15:54:48 +0100

    Add farmer feauture

---
+++ app/Http/Controllers/FarmerController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFarmerRequest;
+use App\Http\Requests\UpdateFarmerRequest;
+use App\Models\Farmer;
+
+class FarmerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFarmerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Farmer $farmer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFarmerRequest $request, Farmer $farmer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Farmer $farmer)
+    {
+        //
+    }
+}
