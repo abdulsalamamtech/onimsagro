c0785d3bffd7a5ec7249c0ce01f9fef8add538fe
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-16 18:34:02 +0100

    Add installation service feature

---
+++ app/Http/Controllers/InstallationServiceController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreInstallationServiceRequest;
+use App\Http\Requests\UpdateInstallationServiceRequest;
+use App\Models\InstallationService;
+
+class InstallationServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInstallationServiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InstallationService $installationService)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInstallationServiceRequest $request, InstallationService $installationService)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InstallationService $installationService)
+    {
+        //
+    }
+}
