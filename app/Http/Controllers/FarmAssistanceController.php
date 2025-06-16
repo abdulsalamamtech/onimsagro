3022d4b2bf91f42c77c508fe510251e08d1de206
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-16 11:45:21 +0100

    Add farm assistance feature

---
+++ app/Http/Controllers/FarmAssistanceController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFarmAssistanceRequest;
+use App\Http\Requests\UpdateFarmAssistanceRequest;
+use App\Models\FarmAssistance;
+
+class FarmAssistanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFarmAssistanceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FarmAssistance $farmAssistance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFarmAssistanceRequest $request, FarmAssistance $farmAssistance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FarmAssistance $farmAssistance)
+    {
+        //
+    }
+}
