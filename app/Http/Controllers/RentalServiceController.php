644ccb078404a20666a8e5b03dd5ed76e5be8f69
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-17 17:45:54 +0100

    Add rental service feature

---
+++ app/Http/Controllers/RentalServiceController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRentalServiceRequest;
+use App\Http\Requests\UpdateRentalServiceRequest;
+use App\Models\RentalService;
+
+class RentalServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRentalServiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RentalService $rentalService)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRentalServiceRequest $request, RentalService $rentalService)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RentalService $rentalService)
+    {
+        //
+    }
+}
