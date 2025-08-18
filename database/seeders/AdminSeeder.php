466141db2b0837e480433f4e37ff626037bebbbe
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-08-18 13:05:19 +0100

    Add admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Developer admin
+        if (!User::where('email', 'abdulsalamamtech@gmail.com')->exists()) {
+            $user = User::create([
+                'name' => 'Admin User',
+                'email' => 'abdulsalamamtech@gmail.com',
+                'password' => bcrypt('password'), // Use bcrypt for password hashing by default
+                'email_verified_at' => now(),
+            ]);
+            $user->assignRole('super-admin');
+        }   
+        // Application admin
+        if (!User::where('email', 'contact@onimsagro.com')->exists()) {
+            $user = User::create([
+                'name' => 'Admin User',
+                'email' => 'contact@onimsagro.com',
+                'password' => bcrypt('password'), // Use bcrypt for password hashing by default
+                'email_verified_at' => now(),
+            ]);
+            $user->assignRole('super-admin');
+        }   
+    }
+}
