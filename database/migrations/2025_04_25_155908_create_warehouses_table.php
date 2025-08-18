31aac3b766b10fe07cc2dd7f9e3fad9a3547166d
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-08-18 11:18:49 +0100

    modified migration file

--- database/migrations/2025_04_25_155908_create_warehouses_table.php
+++ database/migrations/2025_04_25_155908_create_warehouses_table.php
@@ -11,30 +11,30 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Schema::create('warehouses', function (Blueprint $table) {
-        //     // - banner
-        //     // - name
-        //     // - description
-        //     // - price
-        //     // - location
-        //     // - tag [new, trending]
-        //     // - status [active | inactive]
-        //     // - capacity [string]
-        //     $table->id();
-        //     $table->foreignId('banner_id')->nullable()->constrained('assets')->cascadeOnDelete();
-        //     $table->string('name');
-        //     $table->string('description')->nullable();
-        //     $table->string('capacity')->nullable();
-        //     $table->string('sku')->nullable()->unique();
-        //     $table->decimal('price', 10, 2);
-        //     $table->string('tag')->nullable();
-        //     $table->string('location')->nullable();
-        //     // estimated_delivery
-        //     $table->enum('status', ['active', 'inactive'])->default('active');
-        //     $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
-        //     $table->timestamps();
-        //     $table->softDeletes();
-        // });
+          Schema::create('warehouses', function (Blueprint $table) {
+            // - banner
+            // - name
+            // - description
+            // - price
+            // - location
+            // - tag [new, trending]
+            // - status [active | inactive]
+            // - capacity [string]
+            $table->id();
+            $table->foreignId('banner_id')->nullable()->constrained('assets')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('capacity')->nullable();
+            $table->string('sku')->nullable()->unique();
+            $table->decimal('price', 10, 2);
+            $table->string('tag')->nullable();
+            $table->string('location')->nullable();
+            // estimated_delivery
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
     }
 
     /**
