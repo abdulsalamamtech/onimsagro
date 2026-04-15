ff891394546237a25b2dbe21bfa818006c59dab8
abdulsalamamtech <abdulsalamamtech@gmail.com>
2026-04-15 16:50:04 +0100

    feat (bootsrap): modified file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
 
         // Start of customized middleware 
         $middleware->api(prepend: [
